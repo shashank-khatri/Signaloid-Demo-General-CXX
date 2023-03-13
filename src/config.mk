1b8a767360495081889a75032d5cdd1636902284
Shashank Khatri <khatri.shashank@outlook.com>
2023-03-13 14:11:35 +0530

    Config check

--- src/config.mk
+++ src/config.mk
@@ -2,7 +2,7 @@
 
 # Explicitely specify which files to compile
 SOURCES = \
-	main.cpp
+	test.cpp
 
 # Options that get passed to the C compiler
 #CXXFLAGS = -DDEBUG
---
+++ src/test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include <uncertain.h>
+
+int
+main(int argc, char *  argv[])
+{
+	std::vector<double> vec {
+		libUncertainDoubleUniformDist(0.0, 1.0),
+		libUncertainDoubleUniformDist(1.0, 2.0),
+	};
+
+	std::cout << "Initial distributions:" << std::endl;
+	for (const auto& dist : vec)
+	{
+		std::cout << dist << std::endl;
+	}
+
+	std::adjacent_difference(vec.begin(), vec.end(), vec.begin());
+
+	std::cout << "Differences:" << std::endl;
+	for (const auto& diff : vec)
+	{
+		std::cout << diff << std::endl;
+	}
+
+#ifdef DEBUG
+	std::cout << "debug message" << std::endl;
+#endif
+
+	return 0;
+}
+
